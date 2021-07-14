79c9cefef68e72dc6421f3f8db4887fdcfb6dcc9
NatanaelJPS <natanaeljps@gmail.com>
2021-07-14 17:02:00 -0400

    included socials

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function changeHeaderWhenScroll() {
   }
 }
 
-/* Testimonials carousel slider swiper */
+// Testimonials carousel slider swiper 
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
